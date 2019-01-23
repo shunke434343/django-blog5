a2a4d3d7bf7808ba58cf7c9626eb43f692a5c022
shunke434343 <shunka4@gmail.com>
2019-01-23 18:23:45 +0900

    ひとまず

--- project5/urls.py
+++ project5/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog5.urls')),
+    path('tinymce/', include('tinymce.urls')),
 ]
