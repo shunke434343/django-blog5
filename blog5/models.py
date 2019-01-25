9d08f8086390e7c40e579f21e4f7fab79d71e274
shunke434343 <shunka4@gmail.com>
2019-01-25 14:32:39 +0900

    Commit

--- blog5/models.py
+++ blog5/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.utils import timezone
 from tinymce import HTMLField
-# from tinymce import HTMLField
+import datetime
+import pytz
 
-# Create your views here.
 
 class Category(models.Model):
     category = models.CharField('カテゴリー', max_length=25)
@@ -21,16 +21,6 @@ class Tag(models.Model):
         return self.tag
 
 
-class Comment(models.Model):
-    comment = models.TextField('コメント', max_length=300)
-    name = models.CharField('名前', max_length=20, default="")
-    mailadress = models.EmailField('メール', max_length=30, blank=True)
-    created_at = models.DateTimeField('作成日', default=timezone.now)
-
-    def __str__(self):
-        return self.comment
-
-
 class Article(models.Model):
     title = models.CharField('title', max_length=40)
     summary = models.CharField('summary', max_length=100, default="test")
@@ -40,8 +30,26 @@ class Article(models.Model):
     category = models.ForeignKey(Category, verbose_name='カテゴリー', default=11, on_delete=models.PROTECT)
     tag = models.ManyToManyField(Tag, verbose_name='タグ', blank=True)
 
+    def get_days_ago(self):
+        now = datetime.datetime.now()
+        timezone = pytz.timezone('Asia/Tokyo')
+        now = timezone.localize(now)
+        delta = now - self.created_at
+        return delta.days
+
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    comment = models.TextField('コメント', max_length=300)
+    name = models.CharField('名前', max_length=20, default="")
+    mailadress = models.EmailField('メール', max_length=30, blank=True)
+    article = models.ForeignKey(Article, verbose_name='親記事', on_delete=models.PROTECT, default=11)
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+
+    def __str__(self):
+        return self.comment
 
 
 
--- blog5/urls.py
+++ blog5/urls.py
@@ -5,5 +5,8 @@ app_name = 'blog5'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('detail/<int:pk>/', views.DetailView.as_view(), name='detail')
+    path('detail/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('category/<int:pk>/', views.CategoryView.as_view(), name='category'),
+    path('tag/<int:pk>/', views.TagView.as_view(), name='tag'),
+    path('detail/<int:article_id>/comment', views.comment, name='comment')
 ]
