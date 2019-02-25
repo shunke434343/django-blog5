f07bdfe63c6486c19e93d7d201fe67dd7f28fc3b
shunke434343 <shunka4@gmail.com>
2019-02-26 01:07:33 +0900

    Setting

--- blog5/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import Article, Category, Tag
-from markdownx.admin import MarkdownxModelAdmin
-
-admin.site.register(Article, MarkdownxModelAdmin)
-admin.site.register(Category)
-admin.site.register(Tag)
-
-# Register your models here.
--- blog5/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Comment
-
-class CommentCreateForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ('name', 'mailadress', 'comment')
