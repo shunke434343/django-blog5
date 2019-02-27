268a445d0339267557130f774b0bb9bd91e9c3bc
shunke434343 <shunka4@gmail.com>
2019-02-27 19:04:46 +0900

    False

--- project5/settings.py
+++ project5/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-DEBUG = True
+DEBUG = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
